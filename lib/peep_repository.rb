4ad2c531e7206629712299291ac6a32230fe6e67
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-22 13:57:24 +0100

    created create method that creates new user

--- lib/peep_repository.rb
+++ lib/peep_repository.rb
@@ -14,6 +14,23 @@ class PeepRepository
         end
         return peeps
     end
+    def find(id)
+        sql = 'SELECT id, content, time_posted, user_id from peeps where id = $1;'
+        result_set = DatabaseConnection.exec_params(sql,[id])
+        result = result_set[0]
+            peep = Peep.new
+            peep.id = result['id'].to_i
+            peep.content = result['content']
+            peep.time_posted = result['time_posted']
+            peep.user_id = result['user_id']
+        return peep
 
+    end
+    def create(peep)
+        sql = 'INSERT INTO peeps (content, time_posted, user_id) VALUES ($1, $2, $3);'
+        params = [peep.content, peep.time_posted, peep.user_id]
+        sql_params = DatabaseConnection.exec_params(sql,params)
+        return peep
+    end
 
 end
