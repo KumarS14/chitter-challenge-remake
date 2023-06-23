dcafa52121f9ca9798712b932fab82859fbad6cb
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-23 12:55:59 +0100

    created signup route with connection to database

--- app.rb
+++ app.rb
@@ -5,13 +5,16 @@ require_relative 'lib/database_connection'
 require_relative 'lib/peep_repository'
 require_relative 'lib/user_repository'
 
-DatabaseConnection.connect
+DatabaseConnection.connect('chitter_test_database')
 
 class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
     also_reload 'lib/peep_repository'
     also_reload 'lib/user_repository'
+  end
+  get '/signup' do
+    return erb(:signup_page)
   end
 
 
