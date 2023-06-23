03acd8b1867b9317b98410e6b584beb4603f90d3
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-23 13:13:12 +0100

    test if 503 error  returned if nothing is entered

--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -10,11 +10,22 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
   context "GET /" do
-    it 'should allow me to show the signup page' do
-        response = get('/signup')
-        expect(response.status).to eq(200)
-        expect(response.body).to include('<form action="post-signup" type="POST">')
+        it 'should allow me to show the signup page' do
+            response = get('/signup')
+            expect(response.status).to eq(200)
+            expect(response.body).to include('<form action="post-signup" type="POST">')
+
+        end
+    end
+    context "POST /" do
+        it 'Should tell the user that they have not entered in all boxes' do
+            response = post("/post-signup")
+            expect(response.status).to eq(503)
+            expect(response.body).to eq('Signup unsuccessful')
+
+
+        end    
+
 
     end
-end
 end
