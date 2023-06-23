7f504dbbe1e6e0793199a049ef974c0e7eb79901
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-23 13:00:07 +0100

    created test to see if signup page is returned

--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -9,5 +9,12 @@ describe Application do
   # We need to declare the `app` value by instantiating the Application
   # class so our tests work.
   let(:app) { Application.new }
+  context "GET /" do
+    it 'should allow me to show the signup page' do
+        response = get('/signup')
+        expect(response.status).to eq(200)
+        expect(response.body).to include('<form action="post-signup" type="POST">')
 
+    end
+end
 end
