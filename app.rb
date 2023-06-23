13d01466c04a38d8293104a7ba82f4eb2b2cd19b
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-23 13:29:43 +0100

    added validation checks

--- app.rb
+++ app.rb
@@ -17,14 +17,24 @@ class Application < Sinatra::Base
     return erb(:signup_page)
   end
   post '/post-signup' do
-    def invalid_credentials?
+    def invalid_parameters?
       return true if params[:email].nil? || params[:password].nil? || params[:user_name] == nil?
+      return true if params[:email] == "" || params[:password] == "" || params[:user_name] == ""
     end
-
-    if invalid_credentials?
-      status 503
-      return 'Signup unsuccessful'
-    end
+    if invalid_parameters?
+        status 503
+        return status
+     end
+    email = params[:email]
+    user_name = params[:user_name]
+    password = params[:password]
+    repo = UserRepository.new
+    user = User.new
+    user.email = email
+    user.password = password
+    user.user_name = user_name
+    repo.create(user)
+    return 'Signup successful'
   end
   
 
