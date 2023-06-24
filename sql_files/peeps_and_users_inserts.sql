553ee9cce528e7b71aa110593219556c74b804ce
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-24 14:17:56 +0100

    created phcrypto extension

--- sql_files/peeps_and_users_inserts.sql
+++ sql_files/peeps_and_users_inserts.sql
@@ -1,9 +1,9 @@
 TRUNCATE TABLE peeps, users RESTART IDENTITY CASCADE;
 
-INSERT INTO users (email, user_name, password) VALUES ('email1@gmail.com', 'user1','password1');
-INSERT INTO users (email, user_name, password) VALUES ('email2@gmail.com', 'user2', 'password2');
-INSERT INTO users (email, user_name, password) VALUES ('email3@gmail.com', 'user3', 'password3');
-INSERT INTO users (email, user_name, password) VALUES ('email4@gmail.com', 'user4', 'password4');
+INSERT INTO users (email, user_name, password) VALUES ('email1@gmail.com', 'user1', crypt('password1', gen_salt('bf')));
+INSERT INTO users (email, user_name, password) VALUES ('email2@gmail.com', 'user2', crypt('password2', gen_salt('bf')));
+INSERT INTO users (email, user_name, password) VALUES ('email3@gmail.com', 'user3', crypt('password3', gen_salt('bf')));
+INSERT INTO users (email, user_name, password) VALUES ('email4@gmail.com', 'user4', crypt('password4', gen_salt('bf')));
 
 INSERT INTO peeps (content, time_posted, user_id) VALUES ('content1', CURRENT_TIMESTAMP, 1);
 INSERT INTO peeps (content, time_posted, user_id) VALUES ('content2', CURRENT_TIMESTAMP, 2);
